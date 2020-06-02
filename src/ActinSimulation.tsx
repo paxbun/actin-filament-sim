f39e7b71900531bb19937426a821b148cb2b84c4
Chanjung Kim <freiyer.paxbun@gmail.com>
2020-06-02 17:04:08 +0900

    ➕ Add test hard-coded data

--- src/ActinSimulation.tsx
+++ src/ActinSimulation.tsx
@@ -1,42 +1,47 @@
-import ISimulation, { IStatistics, IActin, Vector } from './Simulation';
+import ISimulation, { IStatistics, IActin, Vector } from "./Simulation";
 
 /**
  * the implementation of `ISimulation'
  */
 export default class ActinSimulation implements ISimulation {
+  private actins: IActin[] = [];
 
-    private actins: IActin[] = [];
+  public getCurrentState(): IActin[] {
+    return this.actins;
+  }
 
-    public getCurrentState(): IActin[] {
-        return this.actins;
+  public getCurrentStatistics(): IStatistics {
+    const rtn = new Map<number, number>();
+    rtn.set(10, 20);
+    rtn.set(8, 17);
+    if (Math.random() > 0.5) {
+      rtn.set(5, 25);
     }
+    return {
+      numberByLength: rtn,
+      numberWithAdp: 100 * Math.random(),
+      numberWithAtp: 50 * Math.random(),
+    };
+  }
 
-    public getCurrentStatistics(): IStatistics {
-        return {
-            numberByLength: new Map<number, number>(),
-            numberWithAdp: 100,
-            numberWithAtp: 50,
-        };
-    }
+  public getRadius(): number {
+    return 10;
+  }
 
-    public getRadius(): number {
-        return 10;
+  public precede(deltaTime: number): void {
+    for (const actin of this.actins) {
+      const [x, y] = actin.pos;
+      const [vx, vy] = actin.velocity;
+      actin.pos = [x + vx * deltaTime, y + vy * deltaTime];
     }
+  }
 
-    public precede(deltaTime: number): void {
-        for (const actin of this.actins) {
-            const [x, y] = actin.pos;
-            const [vx, vy] = actin.velocity;
-            actin.pos = [x + vx * deltaTime, y + vy * deltaTime];
-        }
-    }
-
-    public add(pos: Vector): void {
-        this.actins.push({
-            pos: pos,
-            orientation: Math.random() * 2 * Math.PI,
-            velocity: [(Math.random() * 2 - 1) * 10, (Math.random() * 2 - 1) * 10],
-            hasAtp: false
-        });
-    }
+  public add(pos: Vector): void {
+    this.actins.push({
+      pos: pos,
+      orientation: Math.random() * 2 * Math.PI,
+      velocity: [(Math.random() * 2 - 1) * 10, (Math.random() * 2 - 1) * 10],
+      hasAtp: false,
+    });
+  }
 }
