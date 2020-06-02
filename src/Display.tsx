15933f70817e455ef40a0aa2523c074ab03d6899
Chanjung Kim <freiyer.paxbun@gmail.com>
2020-06-02 12:27:36 +0900

    ✨ Complete displaying subunits

--- src/Display.tsx
+++ src/Display.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ISimulation from "./Simulation";
-import "./Display.scss";
+import ISimulation, { IStatistics } from "./Simulation";
 
 /**
  * properties used by `Display`
@@ -23,13 +22,28 @@ export interface IDisplayProperties {
 }
 
 /**
+ * state used by `Display`
+ */
+export interface IDisplayState {
+  /**
+   * Statistics retrieved within the last 20 seconds
+   */
+  statistics: IStatistics[];
+}
+
+/**
  * displays the current state of the simulation
  */
-export default class Display extends React.Component<IDisplayProperties> {
+export default class Display extends React.Component<IDisplayProperties, IDisplayState> {
   /**
-   * the function handler returned by `window.setInterval`
+   * function handler returned by `window.setInterval`; used to update the canvas
    */
   private ticker: number = 0;
+
+  /**
+   * function handler returned by `window.setInterval`; used to update the graphs
+   */
+  private statisticsTicker: number = 0;
 
   /**
    * the last time when `this.props.simulation.precede` was called
@@ -37,9 +51,16 @@ export default class Display extends React.Component<IDisplayProperties> {
   private lastPoint: number = Date.now();
 
   /**
-   * teh canvas to draw
+   * the canvas to draw
    */
   private canvasRef: React.RefObject<HTMLCanvasElement> = React.createRef();
+
+  public constructor(props: IDisplayProperties) {
+    super(props);
+    this.state = {
+      statistics: []
+    };
+  }
 
   public componentDidMount() {
     this.lastPoint = Date.now();
@@ -47,28 +68,43 @@ export default class Display extends React.Component<IDisplayProperties> {
       this.tick();
       this.draw();
     }, 1000 / 60);
+    this.statisticsTicker = window.setInterval(() => {
+      this.updateGraphs();
+    }, 1000);
   }
 
   public componentWillUnmount() {
     window.clearInterval(this.ticker);
+    window.clearInterval(this.statisticsTicker);
     this.ticker = 0;
   }
 
   public render() {
     return (
-      <canvas
-        className="display"
-        ref={this.canvasRef}
-        width={this.props.width}
-        height={this.props.height}
+      <div
         style={{
+          position: "relative",
+          display: "inline-block",
           width: `${this.props.width}px`,
-          height: `${this.props.height}px`
+          height: `${this.props.height}px`,
         }}
         onClick={(event) => {
-          this.props.simulation.add([event.clientX, event.clientY])
+          this.props.simulation.add([event.clientX, event.clientY]);
         }}
-      ></canvas>
+      >
+        <canvas
+          ref={this.canvasRef}
+          width={this.props.width}
+          height={this.props.height}
+          style={{
+            left: "0px",
+            top: "0px",
+            width: "100%",
+            height: "100%",
+            zIndex: 0,
+          }}
+        />
+      </div>
     );
   }
 
@@ -86,22 +122,43 @@ export default class Display extends React.Component<IDisplayProperties> {
    */
   private draw(): void {
     const current = this.props.simulation.getCurrentState();
+    const radius = this.props.simulation.getRadius();
     const c = this.canvasRef.current;
     if (c) {
       const ctx = c.getContext("2d");
-      ctx?.clearRect(0, 0, 1920, 1080);
       if (ctx) {
+        ctx.globalCompositeOperation = "destination-over";
+        ctx.clearRect(0, 0, this.props.width, this.props.height);
         for (const subunit of current) {
           const [x, y] = subunit.pos;
+          const cos = radius * Math.cos(subunit.orientation);
+          const sin = radius * Math.sin(subunit.orientation);
           ctx.beginPath();
-          const grad = ctx.createLinearGradient(x - 20, y - 20, x + 20, y + 20);
+          const grad = ctx.createLinearGradient(
+            x - cos,
+            y - sin,
+            x + cos,
+            y + sin
+          );
           grad.addColorStop(0, "red");
           grad.addColorStop(1, "yellow");
           ctx.fillStyle = grad;
-          ctx.arc(x, y, 20, 0, 2 * Math.PI);
+          ctx.arc(x, y, radius, 0, 2 * Math.PI);
           ctx.fill();
         }
       }
     }
+  }
+
+  /**
+   * Retrieve
+   */
+  private updateGraphs(): void {
+    const statistics = this.state.statistics;
+    statistics.push(this.props.simulation.getCurrentStatistics());
+    if (statistics.length > 20) {
+      statistics.splice(0, 1);
+    }
+    console.log(statistics);
   }
 }
